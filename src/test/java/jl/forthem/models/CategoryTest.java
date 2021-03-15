2839a61f586dfa7ffdaef533515aad7f016559b6
Jean-Louis <jeanlouis.macle@gmail.com>
2021-03-15 04:41:12 -0500

    Added to unit testing

--- src/test/java/jl/forthem/models/CategoryTest.java
+++ src/test/java/jl/forthem/models/CategoryTest.java
@@ -85,4 +85,64 @@ class CategoryTest {
 		{fail("The test of transitivity failed. x_to_y and y_to_z should have been of the same sign ");}
 		
 	}
+	// Requirement 3 for compareTo	
+	// x.compareTo(y)==0 implies that sgn(x.compareTo(z)) == sgn(y.compareTo(z)), for all z.
+	@Test
+	void compareTest_Requirement3()
+	{
+		assertEquals(0, category.compareTo(category_equal_Compare_0));
+		int x_to_z = category.compareTo(category_nonEqual_Compare_non_0_2);
+		int y_to_z = category_equal_Compare_0.compareTo(category_nonEqual_Compare_non_0_2);
+		assertTrue((x_to_z * y_to_z) > 0);
+	}
+	
+	@Test
+	void compareToTest_Same()
+	{
+		assertEquals(0,category.compareTo(category_equal_Compare_0));
+	}
+	
+	@Test
+	void compareToTest_Same2()
+	{	// same name. should return true.
+		assertEquals(0,category.compareTo(category_nonEqual_Compare_0));
+	}
+	
+	/*
+	 * The general contract of hashCode is:
+	 * 
+	 * 1. Whenever it is invoked on the same object more than once 
+	 * during an execution of a Java application, 
+	 * the hashCode method must consistently return the same
+	 * integer, provided no information used in equals comparisons on the object is
+	 * modified. This integer need not remain consistent from one execution of an
+	 * application to another execution of the same application. 
+	 * 
+	 * 2. If two objects are equal according to the equals(Object) method, then calling the hashCode
+	 * method on each of the two objects must produce the same integer result. 
+	 * 
+	 * 3. It is not required that if two objects are unequal according to the
+	 * equals(java.lang.Object) method, then calling the hashCode method on each of
+	 * the two objects must produce distinct integer results. However, the
+	 * programmer should be aware that producing distinct integer results for
+	 * unequal objects may improve the performance of hash tables.
+	 */
+	
+	@Test
+	void hashTest_contract_1()
+	{
+		int hash1 = category.hashCode();
+		int hash2 = category.hashCode();
+		assertEquals(hash1, hash2);
+	}
+	
+	@Test
+	void hashTest_contract_2()
+	{
+		if(category.equals(category_equal_Compare_0))
+		{
+			assertEquals(category.hashCode(), category_equal_Compare_0.hashCode());
+		}
+		else fail("The two items were not identical.");
+	}
 }
