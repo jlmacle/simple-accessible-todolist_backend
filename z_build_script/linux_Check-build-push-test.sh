7af6380378d2f554c4dfb98fa3b5596c601d16cc
jlmacle <jeanlouis.macle@gmail.com>
2021-05-13 14:58:13 +0200

    Build script update.

--- z_build_script/linux_Check-build-push-test.sh
+++ z_build_script/linux_Check-build-push-test.sh
@@ -1,12 +1,12 @@
 #-------------------------------------------------------------------------------------------------------
 # Checking the code quality
 #-------------------------------------------------------------------------------------------------------
-echo "Starting SonarQube"
+echo "SonarQube"
 gnome-terminal -- bash -c 'sonar.sh start; sleep 90'
 echo "Waiting for the SonarQube server to start"
 sleep 90
 
-echo "Running the code quality analysis"
+echo "Code quality analysis"
 gnome-terminal -- bash -c 'cd .. ; mvn sonar:sonar -Dsonar.projectKey=accessible-todo-list_back-end:jl.forthem \
 -Dsonar.host.url=http://localhost:9000 -Dsonar.login=6fb38c8274cb8efccba8778aef56d226e7550659; sleep 100'
 
@@ -22,20 +22,20 @@ sleep 100
 #--------------------------------------------------------------------------------------------------------
 echo "Jar build. The PostgreSQL service must be running."
 sudo service postgresql start
+sleep 60
 
 #removing potential stack services
-sudo docker stack rm stack
+sudo docker stack rm stack &> /dev/null
 cd ..
-pwd
 mvn package
 
-mv -fu target/*.jar z_build_script/context/
+mv -f target/*.jar z_build_script/context/
 echo "Jar file moved in context folder"
 
 #-------------------------------------------------------------------------------------------------------
 # Cleaning the code 
 #-------------------------------------------------------------------------------------------------------
-echo "Running mvn clean"
+echo "mvn clean"
 echo "The value of an apiKey stored in an environment variable has been dumped and later pushed on GitHub."
 echo "The dump file was in the surefire-reports folder. "
 echo "mvn clean suppresses the surefire-reports. "
@@ -44,7 +44,7 @@ mvn clean
 #--------------------------------------------------------------------------------------------------------
 # Pushing the code to GitHub
 #--------------------------------------------------------------------------------------------------------
-echo "git add ."
+echo "git"
 git add .
 echo "git commit: enter a commit message"
 read commit
@@ -61,10 +61,26 @@ sleep 300
 
 echo "Removing a potentially existing image."
 sudo docker service rm atl-back-end &> /dev/null
+sudo docker service rm atl-postgres &> /dev/null
+
+echo "Starting the postgreSQL service"
+sudo docker service create --publish  published=5432,target=5432 --network atl-network --hostname postgresql \
+--secret POSTGRES_PASSWORD -e POSTGRES_PASSWORD_FILE=/run/secrets/POSTGRES_PASSWORD \
+--secret POSTGRES_USER -e POSTGRES_USER_FILE=/run/secrets/POSTGRES_USER \
+--secret POSTGRES_DB -e POSTGRES_DB_FILE=/run/secrets/POSTGRES_DB --name atl-postgres postgres:alpine
+
+echo "Waiting for the postgreSQL service to start"
+sleep 60
 
 echo "Starting the back-end service"
-sudo docker pull jlmacle/back-end:v0.9
-sudo docker service create --publish published=8080,target=8080 --secret POSTGRES_PASSWORD -e POSTGRES_PASSWORD_FILE=/run/secrets/POSTGRES_PASSWORD --secret POSTGRES_USER -e POSTGRES_USER_FILE=/run/secrets/POSTGRES_USER --secret POSTGRES_DB -e POSTGRES_DB_FILE=/run/secrets/POSTGRES_DB --secret DB_JDBC_ROOT -e DB_JDBC_ROOT_FILE=/run/secrets/DB_JDBC_ROOT --name atl-back-end jlmacle/back-end:v0.9
+cd
+sudo docker service create --publish published=8080,target=8080 \
+--secret POSTGRES_PASSWORD -e POSTGRES_PASSWORD_FILE=/run/secrets/POSTGRES_PASSWORD \
+--secret POSTGRES_USER -e POSTGRES_USER_FILE=/run/secrets/POSTGRES_USER \
+--secret POSTGRES_DB -e POSTGRES_DB_FILE=/run/secrets/POSTGRES_DB 
+--secret DB_JDBC_ROOT -e DB_JDBC_ROOT_FILE=/run/secrets/DB_JDBC_ROOT --name atl-back-end jlmacle/back-end:v0.9
 
+echo "Waiting for the back-end to start"
+chromium-browser http:// http://localhost:8080
 
 
