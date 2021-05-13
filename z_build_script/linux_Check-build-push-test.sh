099411dae4542e68a2d92bc84ffcb66746fcbc2a
jlmacle <jeanlouis.macle@gmail.com>
2021-05-13 15:46:58 +0200

    Dockerhub test

--- z_build_script/linux_Check-build-push-test.sh
+++ z_build_script/linux_Check-build-push-test.sh
@@ -29,7 +29,7 @@ sudo docker stack rm stack &> /dev/null
 cd ..
 mvn package
 
-mv -f target/*.jar z_build_script/context/
+mv -uf target/*.jar z_build_script/context/
 echo "Jar file moved in context folder"
 
 #-------------------------------------------------------------------------------------------------------
@@ -58,6 +58,7 @@ git push
 #--------------------------------------------------------------------------------------------------------
 echo "Waiting for the image update."
 sleep 300
+sudo docker pull jlmacle/atl-back-end:v0.9
 
 echo "Removing a potentially existing image."
 sudo docker service rm atl-back-end &> /dev/null
@@ -70,7 +71,7 @@ sudo docker service create --publish  published=5432,target=5432 --network atl-n
 --secret POSTGRES_DB -e POSTGRES_DB_FILE=/run/secrets/POSTGRES_DB --name atl-postgres postgres:alpine
 
 echo "Waiting for the postgreSQL service to start"
-sleep 60
+sleep 90
 
 echo "Starting the back-end service"
 cd
@@ -78,7 +79,7 @@ sudo docker service create --publish published=8080,target=8080 \
 --secret POSTGRES_PASSWORD -e POSTGRES_PASSWORD_FILE=/run/secrets/POSTGRES_PASSWORD \
 --secret POSTGRES_USER -e POSTGRES_USER_FILE=/run/secrets/POSTGRES_USER \
 --secret POSTGRES_DB -e POSTGRES_DB_FILE=/run/secrets/POSTGRES_DB \
---secret DB_JDBC_ROOT -e DB_JDBC_ROOT_FILE=/run/secrets/DB_JDBC_ROOT --name atl-back-end jlmacle/back-end:v0.9
+--secret DB_JDBC_ROOT -e DB_JDBC_ROOT_FILE=/run/secrets/DB_JDBC_ROOT --name atl-back-end jlmacle/atl-back-end:v0.9
 
 echo "Waiting for the back-end to start"
 chromium-browser http://localhost:8080
