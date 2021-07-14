a7886ea91eaebd2bbd69c766bad5b03bfe57cfd0
jlmacle <jeanlouis.macle@gmail.com>
2021-07-14 11:47:29 +0200

    Script for rapid testing with local images.

--- z_build_script/linux-No_code_quality_test_version-build_backend_locally_and_start_app_with_local_images.sh
+++ z_build_script/linux-No_code_quality_test_version-build_backend_locally_and_start_app_with_local_images.sh
@@ -1,45 +1,57 @@
-echo "Starting the test with a local back-end image built."
-
-echo "Suppressing a potentially existing stack."
-sudo docker stack rm stack &>/dev/null
-echo "Waiting for a potential stack to be removed."
-sleep 20
-
-echo "Suppressing a potentially existing Docker postgres service."
-sudo docker service rm atl-postgres &>/dev/null
-echo "Waiting for the potential Docker postgres service to be removed."
-sleep 20
-
-echo "Suppressing a potentially existing Docker front-end service."
-sudo docker service rm atl-front-end &>/dev/null
-echo "Waiting for the potential Docker atl-front-end service to be removed."
-sleep 20
-
-echo "Suppressing a potentially existing Docker atl-back-end service."
-sudo docker service rm atl-back-end &>/dev/null
-echo "Waiting for the potential Docker atl-back-end service to be removed."
-sleep 20
-
-echo "Demarrage du service postgresql" 
-sudo service postgresql start
+echo "* Making sure that no PostgreSQL Ubuntu service is running."
+sudo service postgresql stop
+echo " Waiting for a potential PostgreSQL Ubuntu service to stop."
 sleep 60
 
-echo "Remaining Docker services:"
-sudo docker service ls
-
 cd ..
+
+echo "* Preparing for the Docker build."
+#--------------------------------------------------------------------------------------------------------
+# Building the jar file
+#--------------------------------------------------------------------------------------------------------
+echo "****** Preparing for the jar file building."
+echo "****** ****** Suppressing a potential stack_backend service to release the port 8080 before using mvn package."
+sudo docker service rm stack_backend
+
+echo "****** ****** Pruning the Docker system to avoid unexpected side-effects that could involve port 8080."
+sudo docker system prune -f
+
+echo "****** ****** Verifying the remaining Docker services for a potential use of port 8080."
+sudo docker service ls
+sleep 20
+
+echo "****** Building the jar file with mvn package."
 mvn package
 
-sudo service postgresql stop
+echo "****** Moving the jar file in the context folder."
+mv -uf target/*.jar z_build_script/context/
+echo "Jar file moved in context folder."
 
+echo "* Building the Docker image."
 mv -uf target/*.jar z_build_script/context/
 echo "Jar file moved in context folder"
 
-cd z_build_script/context
-sudo docker build -t atl-back-end:v0.9 .
+#--------------------------------------------------------------------------------------------------------
+# Testing the new image built
+#--------------------------------------------------------------------------------------------------------
 
-cd ../..
+echo "* Updating the potential stack."
 sudo docker stack deploy -c z_docker_compose/docker-compose-stack-localImages.yml stack
-sleep 30
+sleep 60
+echo "Waiting for the stack to be built."
 
-chromium-browser http://127.0.0.1:8080
+echo "* Starting Chromium to check on the result."
+chromium-browser http://127.0.0.1:8080 & chromium-browser http://127.0.0.1/index.html &
+
+echo "* Clearing the code from the surefire-reports folder."
+#-------------------------------------------------------------------------------------------------------
+# Cleaning the code 
+#-------------------------------------------------------------------------------------------------------
+echo "mvn clean"
+echo "The value of an apiKey stored in an environment variable has been dumped and later pushed on GitHub."
+echo "The dump file was in the surefire-reports folder. "
+echo "mvn clean suppresses the surefire reports. "
+mvn clean
+
+
+
