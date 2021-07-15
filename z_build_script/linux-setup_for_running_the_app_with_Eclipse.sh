7a1775908c4b8069a9b6c01ac61c2ba64c1e6760
jlmacle <jeanlouis.macle@gmail.com>
2021-07-15 13:19:53 +0200

    Added a script to prepare for a run of the back-end app with Eclipse.

--- z_build_script/linux-setup_for_running_the_app_with_Eclipse.sh
+++ z_build_script/linux-setup_for_running_the_app_with_Eclipse.sh
@@ -1,10 +1,25 @@
 echo "* Removing potential stack_backend service."
 sudo docker service rm stack_backend 2> /dev/null
 
-echo "* Removing potential stack_postgresql service."
-sudo docker service rm stack_postgresql 2> /dev/null
-echo "Waiting for the PostgreSQL service to stop."
-sleep 20
+echo "* Testing if a Docker PostgreSQL service is already running."
+noAtlPostgresServiceFound=$(sudo docker service ls | grep atl-postgres)
+noStackPostgresqlServiceFound=$(sudo docker service ls | grep stack_postgresql)
 
-echo "* Starting the Ubuntu PostgreSQL service"
-sudo service postgresql start
+echo $noAtlPostgresServiceFound
+echo $noStackPostgresqlServiceFound
+echo ""
+# https://www.gnu.org/savannah-checkouts/gnu/bash/manual/bash.html#Conditional-Constructs
+# https://www.gnu.org/savannah-checkouts/gnu/bash/manual/bash.html#Bash-Conditional-Expressions
+# -z string: True if the length of string is zero.
+if  ([ -z "$noAtlPostgresServiceFound" ] && [ -z "$noStackPostgresqlServiceFound" ]);
+	then
+		echo "***** No Docker PostgreSQL service is running. Starting a Docker PostgreSQL service."
+		sudo docker service create --publish published=5432,target=5432 \
+		--secret POSTGRES_PASSWORD -e POSTGRES_PASSWORD_FILE=/run/secrets/POSTGRES_PASSWORD \
+		--secret POSTGRES_USER -e POSTGRES_USER_FILE=/run/secrets/POSTGRES_USER \
+		--secret POSTGRES_DB -e POSTGRES_DB_FILE=/run/secrets/POSTGRES_DB \
+		--name atl-postgres postgres:alpine		
+		
+	else
+		echo "***** A Docker PostgreSQL service is already running."			
+fi
