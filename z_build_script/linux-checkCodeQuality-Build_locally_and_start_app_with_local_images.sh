9da1350ce4534ed70f6595d7d67eda8014f6f884
jlmacle <jeanlouis.macle@gmail.com>
2021-07-16 18:16:20 +0200

    Modified the script to use mvn spring-boot:build-image

--- z_build_script/linux-checkCodeQuality-Build_locally_and_start_app_with_local_images.sh
+++ z_build_script/linux-checkCodeQuality-Build_locally_and_start_app_with_local_images.sh
@@ -24,43 +24,27 @@ echo "****** Starting a browser to check the result of the analysis."
 chromium-browser http://localhost:9000 &
 sleep 100
 
-echo "* Preparing for the Docker build."
-#--------------------------------------------------------------------------------------------------------
-# Building the jar file
-#--------------------------------------------------------------------------------------------------------
-echo "****** Preparing for the jar file building."
-echo "****** ****** Suppressing a potential stack_backend service to release the port 8080 before using mvn package."
-sudo docker service rm stack_backend
+echo "* Docker build process."
+echo "**** Login to Docker if necessary."
+sudo docker login
 
-echo "****** ****** Pruning the Docker system to avoid unexpected side-effects that could involve port 8080."
-sudo docker system prune -f
+echo "**** Building the jar file with."
+sudo mvn spring-boot:build-image
 
-echo "****** ****** Verifying the remaining Docker services for a potential use of port 8080."
-sudo docker service ls
-sleep 20
-
-echo "****** Building the jar file with mvn package."
-mvn package
-
-echo "****** Moving the jar file in the context folder."
-mv -uf target/*.jar z_build_script/context/
-echo "Jar file moved in context folder."
-
-echo "* Building the Docker image."
-mv -uf target/*.jar z_build_script/context/
-echo "Jar file moved in context folder"
+echo "**** Re-taging the Docker image"
+sudo docker tag accessible-todo-list_back-end:0.0.1-SNAPSHOT atl-back-end:v0.9
 
 #--------------------------------------------------------------------------------------------------------
 # Testing the new image built
 #--------------------------------------------------------------------------------------------------------
 
-echo "* Updating the potential stack."
+echo "* Deploying with Docker stack."
 sudo docker stack deploy -c z_docker_compose/docker-compose-stack-localImages.yml stack
 sleep 60
 echo "Waiting for the stack to be built."
 
 echo "* Starting Chromium to check on the result."
-chromium-browser http://127.0.0.1:8080 & chromium-browser http://127.0.0.1/index.html &
+chromium-browser http://127.0.0.1:8080 &
 
 echo "* Clearing the code from the surefire-reports folder."
 #-------------------------------------------------------------------------------------------------------
