ca91012628c36f510980c6d34dd8acd8e94506b6
jlmacle <jeanlouis.macle@gmail.com>
2021-07-23 13:11:09 +0200

    Code cleanup.

--- z_build_script/linux-no_code_quality_test_version-build_backend_locally_and_start_app_with_local_images.sh
+++ z_build_script/linux-no_code_quality_test_version-build_backend_locally_and_start_app_with_local_images.sh
@@ -2,12 +2,12 @@
 # Preparing for the new image build
 #--------------------------------------------------------------------------------------------------------
 
-echo "* Making sure that no PostgreSQL Ubuntu service is running."
+echo "** Making sure that no PostgreSQL Ubuntu service is running. **"
 sudo service postgresql stop
 echo "**** Waiting for a potential PostgreSQL Ubuntu service to stop."
 sleep 60
 
-echo "* Making sure that no Docker stack is running (Solved a Docker back-end image update issue)."
+echo "** Making sure that no Docker stack is running (Solved a Docker back-end image update issue). **"
 sudo docker stack rm stack 2> /dev/null
 echo "**** Waiting for a potential Docker stack to be removed."
 sleep 60
@@ -18,7 +18,7 @@ cd ..
 # Building the new image 
 #--------------------------------------------------------------------------------------------------------
 
-echo "* Docker build process."
+echo "** Docker build process. **"
 echo "**** Login to Docker if necessary."
 sudo docker login
 
@@ -35,13 +35,13 @@ sudo docker tag accessible-todo-list_back-end:0.0.1-SNAPSHOT atl-back-end:v0.9
 # Testing the new image built
 #--------------------------------------------------------------------------------------------------------
 
-echo "* Deploying with Docker stack."
+echo "** Deploying with Docker stack. **"
 echo "**** Using docker stack deploy."
 sudo docker stack deploy -c z_docker_compose_files/docker-compose-stack-localImages.yml stack
 sleep 60
 echo "**** Waiting for the stack to be built."
 
-echo "* Starting Chromium to check on the result."
+echo "** Starting Chromium to check on the result. **"
 chromium-browser http://127.0.0.1:8080 &
 
 
--- z_build_script/linux-tag_and_push_image.sh
+++ z_build_script/linux-tag_and_push_image.sh
@@ -1,6 +1,6 @@
-echo "* docker login."
+echo "** docker login. **"
 sudo docker login
-echo "* docker tag."
+echo "** docker tag. **"
 sudo docker tag atl-back-end:v0.9  jlmacle/atl-back-end:v0.9
-echo "* docker push."
+echo "** docker push. **"
 sudo docker push jlmacle/atl-back-end:v0.9
