603ee5a4e670b5dc62e0b97ba2f067d315838927
Jean-Louis <jeanlouis.macle@gmail.com>
2021-07-25 22:37:57 +0200

    macOS: script cleanup of code removing services potentially concurrent to running the back-end with Spring Boot.

--- z_before_running_the_SpringBoot_app/macos-cleaning_up_potentially_concurrent_services.zsh
+++ z_before_running_the_SpringBoot_app/macos-cleaning_up_potentially_concurrent_services.zsh
@@ -1,5 +1,9 @@
-echo "**** ISSUE: the script needs to be finished. ****"
-echo "** Removing potential stack_backend service using port 8080. **"
-sudo docker service rm stack_backend 2> /dev/null
-echo "** Removing potential stack_postgresql service using port 5432.**"
-sudo docker service rm stack_postgresql 2> /dev/null
+echo "**** Running the back-end as a Spring Boot app assumes for a regular, non-Docker PostgreSQL service to be running.
+(to use localhost, instead of postgresql, as hostname)  ****"
+
+echo "** Removing potential stack_backend service using port 8080 and potential stack_postgresql service using port 5432.**"
+sudo docker stack rm stack 2> /dev/null
+
+echo "** Removing potential atl-postgres and atl-backend services. **"
+sudo docker service rm atl-postgres 2> /dev/null
+sudo docker service rm atl-back-end 2> /dev/null
