3bde25379a7bb58071c0c5c238a7b77b54923ca7
Jean-Louis Macle <59054324+jlmacle@users.noreply.github.com>
2021-07-25 15:56:06 -0500

    Delete windows-cleaning_up_potentially_concurrent_services.bat

--- z-before_running_the_app/windows-cleaning_up_potentially_concurrent_services.bat
+++
@@ -1,5 +0,0 @@
-echo "**** ISSUE: the script needs to be finished. ****"
-echo "** Removing potential stack_backend service using port 8080. **"
-sudo docker service rm stack_backend 2> /dev/null
-echo "** Removing potential stack_postgresql service using port 5432.**"
-sudo docker service rm stack_postgresql 2> /dev/null
