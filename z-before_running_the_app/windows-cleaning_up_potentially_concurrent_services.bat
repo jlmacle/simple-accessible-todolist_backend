e890575b255660b8d9b05de30a7e4f6b4590aeb9
unknown <jeanlouis.macle@gmail.com>
2021-07-25 11:19:40 -0500

    Windows script removing  potentially concurrent services before starting the back-end

--- z-before_running_the_app/windows-cleaning_up_potentially_concurrent_services.bat
+++ z-before_running_the_app/windows-cleaning_up_potentially_concurrent_services.bat
@@ -1,5 +1,32 @@
-echo "**** ISSUE: the script needs to be finished. ****"
-echo "** Removing potential stack_backend service using port 8080. **"
-sudo docker service rm stack_backend 2> /dev/null
-echo "** Removing potential stack_postgresql service using port 5432.**"
-sudo docker service rm stack_postgresql 2> /dev/null
+cls
+@echo off
+
+echo ** Starting Docker Desktop. **
+cmd /c "C:\Program Files\Docker\Docker\Docker Desktop.exe"
+echo **** Waiting for Docker Desktop to start
+timeout /T 60
+
+echo ** Removing potential stack_backend service using port 8080. **
+docker service rm stack_backend 2> NUL
+echo ** Removing potential stack_postgresql service using port 5432.**
+docker service rm stack_postgresql 2> NUL
+
+echo ** Testing if a Docker atl-postgres service is already running. **
+(docker service ls | findstr atl-postgres) > tmp_info.txt
+:: issue without the following line.
+set isAtlPostgresServiceFound=
+set /p isAtlPostgresServiceFound=< tmp_info.txt
+set isAtlPostgresServiceFound="%isAtlPostgresServiceFound%"
+del tmp_info.txt
+
+IF %isAtlPostgresServiceFound% == "" (echo ***** No Docker atl-postgres service is running. Starting a Docker PostgreSQL service.
+		docker service create --publish published=5432,target=5432 --secret POSTGRES_PASSWORD -e POSTGRES_PASSWORD_FILE=/run/secrets/POSTGRES_PASSWORD --secret POSTGRES_USER -e POSTGRES_USER_FILE=/run/secrets/POSTGRES_USER --secret POSTGRES_DB -e POSTGRES_DB_FILE=/run/secrets/POSTGRES_DB --name atl-postgres postgres:alpine ) ELSE (echo ***** A Docker atl-postgres service is already running.
+		echo The back-end should be able to be run successfully.)
+
+
+
+
+
+
+
+
