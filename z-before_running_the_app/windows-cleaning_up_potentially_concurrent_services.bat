6345fe589029c798f649c8dde727b453996d9d6e
unknown <jeanlouis.macle@gmail.com>
2021-07-28 04:09:01 -0500

    script testing: code added in comments.

--- z-before_running_the_app/windows-cleaning_up_potentially_concurrent_services.bat
+++ z-before_running_the_app/windows-cleaning_up_potentially_concurrent_services.bat
@@ -1,32 +1,16 @@
 cls
 @echo off
-
+echo **** Running the back-end as a Spring Boot app assumes for a regular, non-Docker PostgreSQL service to be running.
+(to use localhost, instead of postgresql, as hostname)  ****
 echo ** Starting Docker Desktop. **
 cmd /c "C:\Program Files\Docker\Docker\Docker Desktop.exe"
 echo **** Waiting for Docker Desktop to start
 timeout /T 60
 
-echo ** Removing potential stack_backend service using port 8080. **
-docker service rm stack_backend 2> NUL
-echo ** Removing potential stack_postgresql service using port 5432.**
-docker service rm stack_postgresql 2> NUL
+** Removing potential stack_backend service using port 8080 and potential stack_postgresql service using port 5432.**
 
-echo ** Testing if a Docker atl-postgres service is already running. **
-(docker service ls | findstr atl-postgres) > tmp_info.txt
-:: issue without the following line.
-set isAtlPostgresServiceFound=
-set /p isAtlPostgresServiceFound=< tmp_info.txt
-set isAtlPostgresServiceFound="%isAtlPostgresServiceFound%"
-del tmp_info.txt
-
-IF %isAtlPostgresServiceFound% == "" (echo ***** No Docker atl-postgres service is running. Starting a Docker PostgreSQL service.
-		docker service create --publish published=5432,target=5432 --secret POSTGRES_PASSWORD -e POSTGRES_PASSWORD_FILE=/run/secrets/POSTGRES_PASSWORD --secret POSTGRES_USER -e POSTGRES_USER_FILE=/run/secrets/POSTGRES_USER --secret POSTGRES_DB -e POSTGRES_DB_FILE=/run/secrets/POSTGRES_DB --name atl-postgres postgres:alpine ) ELSE (echo ***** A Docker atl-postgres service is already running.
-		echo The back-end should be able to be run successfully.)
-
-
-
-
-
-
-
-
+echo ** Removing potential stack_backend service using port 8080 and potential stack_postgresql service using port 5432.**
+docker stack rm stack 2> NUL
+echo ** Removing potential atl-postgres and atl-backend services. **
+docker service rm atl-postgres 2> NUL
+docker service rm atl-back-end 2> NUL
