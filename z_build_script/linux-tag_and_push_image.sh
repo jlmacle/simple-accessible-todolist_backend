881cfdcf705fdb195b56ded4460c1670bd68df00
unknown <jeanlouis.macle@gmail.com>
2021-07-29 07:55:29 -0500

    style: folder name renaming.

--- z_build_script/linux-tag_and_push_image.sh
+++
@@ -1,6 +0,0 @@
-echo "** docker login. **"
-sudo docker login
-echo "** docker tag. **"
-sudo docker tag atl-back-end:v0.9  jlmacle/atl-back-end:v0.9
-echo "** docker push. **"
-sudo docker push jlmacle/atl-back-end:v0.9
